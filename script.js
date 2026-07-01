c5f00e898d6f0dc0d53dde672a5e1e67a2ce8aaa
dapskuy-web <dapuy2157@gmail.com>
2026-07-02 02:38:15 +0700

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // KONFIGURASI GIST
 // ==========================================================
 const GIST_ID = 'dde9e0ca0b50e38f9b2af7e44b5686bb';
-const GIST_TOKEN = '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';
+const GIST_TOKEN = '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';
 
 // ==========================================================
 // STATE
